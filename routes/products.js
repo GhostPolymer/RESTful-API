a6bbc8a9ae432d15ab5858aaef6d72768d0567f3
Hunter Rafuse <31886722+GhostPolymer@users.noreply.github.com>
2018-03-16 17:03:10 +0200

    Create products.js
    Original products commit

---
+++ routes/products.js
@@ -0,0 +1,150 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+
+
+const Product = require('../models/product');
+//get all the DOCS
+router.get('/', (req, res, next) =>{
+  Product.find()
+  .select('name price _id')
+  .exec()
+  .then(docs => {
+      const response = {
+        count: docs.length,
+        products: docs.map(doc => {
+          return{
+            name: doc.name,
+            price: doc.price,
+            _id: doc._id,
+            request: {
+              type: 'GET',
+              url: 'http://localhost:3000/products/' + doc._id  //GET DATA FROM API URL
+            }
+          }
+        });
+      };
+    // if (docs.length >= 0) {
+      res.status(200).json(docs);
+    // } else {
+    //   res.status(404).json({
+    //     message: "No entries found. Try again."
+    //   });
+    // }
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json({
+      error: err
+    });
+  });
+});
+//post route(create new products)
+router.post('/', (req, res, next) =>{
+  const product = new Product({
+    _id: new mongoose.Types.ObjectId(),
+    name: req.body.name,
+    price: req.body.price
+  });
+  product
+  .save().then(result =>{
+    console.log(result);
+    res.status(201).json({
+      message: 'Created product successfully within the system',
+      createdProduct: {
+        name: result.name,
+        price: result.price,
+        _id: result._id,
+        request: {
+          type: 'GET',
+          url: 'http://localhost:3000/products/' + result._id
+        }
+      }
+    });
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json({
+      error: err
+    });
+  });
+//get route
+router.get('/:productId', (req, res, next) =>{
+  const id = req.params.productId;
+  Product.findById(id)
+  .select('name price _id')
+  .exec()
+  .then(doc => {
+    console.log("From database", doc);
+    if (doc) {
+      res.status(200).json(doc);
+        product: doc,
+        request: {
+          type: 'GET',
+          description: 'GET ALL PRODUCTS',
+          url: 'http://localhost:3000/products'
+        }
+    } else {
+      res.status(404).json({
+        message: 'No valid entry found for provided ID'
+      })
+    }
+    .catch(err => {
+      console.log(err);
+      rest.status(500).json({
+        error: err
+      });
+    });
+  });
+
+//patch route
+router.patch('/:productId', (req, res, next) =>{
+      const id = req.params.productId;
+      const updateOps = {};
+      for (const ops of req.body) {
+        updateOps[ops.propName] = ops.value;
+      }
+    Product.update({_id: id}, { $set updateOps})
+    .select()
+    .exec()
+    .then( result=> {
+      res.status(200).json({
+        message: 'PRODUCT UPDATED',
+        request: 'GET',
+        url: 'http://localhost:3000/products' + id
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+      
+    });
+});
+
+//delete route
+router.delete('/:productId', (req, res, next) =>{
+      const id = req.params.productId;
+    Product.remove({_id: id})
+    .exec()
+    .then(result => {
+      res.status(200).json({
+        message: 'Product deleted!',
+        request: {
+          type: 'POST',
+          url: 'http://localhost:3000/products/',
+          body: {name: 'String', price: 'Number'}
+        }
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+    });
+});
+
+
+module.exports = router;
